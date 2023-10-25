047a0350211f6e4cc0f012d76b47fa1744264f2e
Jonathan Varghese <jonathanvarghese24@gmail.com>
2023-10-25 10:00:23 -0500

    Initial Commit

---
+++ 04 Roll the dice/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  04 Roll the dice
+//
+//  Created by Jonathan Varghese on 10/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 04 Roll the dice/_4_Roll_the_diceApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4_Roll_the_diceApp.swift
+//  04 Roll the dice
+//
+//  Created by Jonathan Varghese on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct _4_Roll_the_diceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
